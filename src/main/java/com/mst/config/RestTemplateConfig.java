ffb3d1bd7481a79c72e4be05342d1b75b0930b41
omarbridgetech <38240168+omarbridgetech@users.noreply.github.com>
2025-11-15 13:54:02 +0200

    add RestTemplateConfig

---
+++ src/main/java/com/mst/config/RestTemplateConfig.java
@@ -0,0 +1,23 @@
+package com.mst.config;
+
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfig {
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(30000); // 30 seconds
+        factory.setReadTimeout(30000); // 30 seconds
+
+        return builder
+                .requestFactory(() -> factory)
+                .build();
+    }
+}
