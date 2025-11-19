8bebf9463dba393e3da77210eee7af3ff097ea95
omarbridgetech <38240168+omarbridgetech@users.noreply.github.com>
2025-11-19 17:41:02 +0200

    initial commit for Sms branch

---
+++ src/test/java/com/mst/smsms/SmsMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mst.smsms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
