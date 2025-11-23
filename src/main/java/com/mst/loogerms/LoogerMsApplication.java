dbe94b88b97edde63a1efe50f462690a427f5840
omarbridgetech <38240168+omarbridgetech@users.noreply.github.com>
2025-11-23 17:35:37 +0200

    initial commit

---
+++ src/main/java/com/mst/loogerms/LoogerMsApplication.java
@@ -0,0 +1,13 @@
+package com.mst.loogerms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoogerMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoogerMsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mst/loogerms/LoogerMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mst.loogerms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoogerMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
