229009c9b204d839ff09209f0f3bd987121ba51d
omarbridgetech <38240168+omarbridgetech@users.noreply.github.com>
2025-11-25 16:19:24 +0200

    Add @Transactional annotation to loadUserByUsername

--- src/main/java/com/Alnsor/demo/security/CustomUserDetailsService.java
+++ src/main/java/com/Alnsor/demo/security/CustomUserDetailsService.java
@@ -25,6 +25,7 @@ public class CustomUserDetailsService implements UserDetailsService {
     }
 
     @Override
+    @Transactional
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
